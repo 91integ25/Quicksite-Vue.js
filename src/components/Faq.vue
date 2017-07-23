c57117e8b5d1b91e8a05c1a85f0cad914613cd43
91integ25 <91integ25@gmail.com>
2017-07-23 11:34:33 -0400

    adding sass files

---
+++ src/components/Faq.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="faq">
+   faq
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'faq',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import "../mq"
+</style>
