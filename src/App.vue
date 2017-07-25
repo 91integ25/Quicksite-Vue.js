909e153133e0f549fcfb5f998a7c307758a92bb6
91integ25 <91integ25@gmail.com>
2017-07-25 19:38:18 -0400

    added mobile menu

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
         <div class="nav-left">
           <a class="nav-item">MyCompany</a>
         </div>
-        <span class="nav-toggle">
+        <span class="nav-toggle" v-on:click="toggleNav" v-bind:class ="{'is-active': isActive}">
           <span></span>
           <span></span>
           <span></span>
         </span>
-        <div class="nav-right nav-menu">
+        <div class="nav-right nav-menu" v-bind:class ="{'is-active': isActive}">
           <router-link to="/" class="nav-item r-item">Home</router-link>
           <router-link to="/faq" class="nav-item r-item">Features</router-link>
           <router-link to="/faq" class="nav-item r-item">About</router-link>
@@ -36,8 +36,19 @@
 </template>
 
 <script>
+
 export default {
-  name: 'app'
+  name: 'app',
+  data:function(){
+    return{
+      isActive:false
+    }
+  },
+  methods: {
+    toggleNav: function(){
+      this.isActive = !this.isActive;
+    }
+  }
 }
 </script>
 
@@ -49,15 +60,17 @@ export default {
   background-color:#383838
   a:hover
     color: gray
+
 .nav-left a
   color:#fff
   font-weight: bold
-a.r-item
+a .r-item
   color:#C1C1C1
   padding: 0.5rem 1.75rem
   +mobile
-  color: gray
-  &:hover
-    background-color: #F1F1F1
-
+    color: gray
+    &:hover
+      background-color: #F1F1F1
+.nav-toggle span
+  background-color:#C1C1C1
 </style>
