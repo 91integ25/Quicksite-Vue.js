9231aa9ca76b8572357bb9216560e2fce22ad581
91integ25 <91integ25@gmail.com>
2017-08-21 19:43:55 -0400

    homepage started

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
-   Home
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title"></h1>
+          <div class="is-two-thirds column ispaddingless">
+            <h2 class="subtitle is-4"></h2>
+            <a href="" class="button is-large is-primary"></a>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
-
 <script>
 export default {
   name: 'home',
-
+  data(){ return{
+      heading:"Soaring to new heights",
+      subheading:"Lorem ipsum"
+    }
+  }
 }
 </script>
 
