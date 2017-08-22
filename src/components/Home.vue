e8409271c7531a933a67f21b48e157f1924373af
91integ25 <91integ25@gmail.com>
2017-08-22 19:07:16 -0400

    finished home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,54 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title"></h1>
-          <div class="is-two-thirds column ispaddingless">
-            <h2 class="subtitle is-4"></h2>
-            <a href="" class="button is-large is-primary"></a>
+          <h1 class="title">{{heading}}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{subheading}}</h2>
+
+          </div>
+           <a href="" class="button is-large is-primary" id="learn">Learn More</a>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary">
+
+            </i>
+          </div>
+          <div class="column is-one-third-desktop">
+            <p class="title"><strong>We provide superior logistics so that your business can succeed in a crazy world.</strong></p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+          </div>
+        </div>
+      </div>
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">“There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</p>
+              <p class="subtitle">-Gary simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">“There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</p>
+              <p class="subtitle">-Gary simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">“There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</p>
+              <p class="subtitle">-Gary simon</p>
+            </div>
           </div>
         </div>
       </div>
@@ -18,7 +62,7 @@ export default {
   name: 'home',
   data(){ return{
       heading:"Soaring to new heights",
-      subheading:"Lorem ipsum"
+      subheading:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
   }
 }
@@ -27,4 +71,27 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import "../mq"
+.hero
+  background: url("../assets/clouds.jpg")
+  background-size: cover
+
+  .title
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4 rem
+      margin-top: 2rem
+h2
+  margin: 1.5rem 0 2rem 0 !important
+.fa-cog
+  font-size: 40px
+#learn
+  +desktop
+    margin-bottom: 2rem
+.pd
+  +tablet
+    padding: 2em 0
+
 </style>
